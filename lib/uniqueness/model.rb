f401d6ac8480fd9a52aa21f070c87a9819a6c9fb
Gaston Morixe <gaston@black.uy>
2017-12-24 17:56:49 -0300

    Update model.rb

--- lib/uniqueness/model.rb
+++ lib/uniqueness/model.rb
@@ -49,7 +49,7 @@ module Uniqueness
     end
 
     def uniqueness_validation
-      return self.new_record?      
+      return if self.new_record?      
       self.class.uniqueness_options.each do |field, options|
         value = send(field)
         if value.nil?
